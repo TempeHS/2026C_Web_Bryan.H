e9be079367454135872bb7e58fceb11543485b14
Bryan H <bryan.huang1@education.nsw.gov.au>
2025-05-05 02:49:55 +0000

    added menu and stuff

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def Hello_world():
-    backend_data = "bryan"
-    return render_template("index.html",front_end_data = backend_data), 200
+@app.route('/index.html')
+def index():
+    return render_template("index.html"), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
