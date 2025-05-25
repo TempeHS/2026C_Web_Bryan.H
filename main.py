3703c2ee521bf4e2706f44df7ec61bdc68ac9be5
Bryan H <bryan.huang1@education.nsw.gov.au>
2025-05-25 23:36:31 +0000

    cards at that are working

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index.html')
 def index():
+    card_data = (
+        ("Meet the skibidis", "Big library of skibidi", "Yessir!", "static/images/logo.png")
+        ("who are they?", "Big library of skibidi", "Yessir!", "static/images/logo.png")
+        ("Meet the skibidis", "Big library of skibidi", "Yessir!", "static/images/logo.png")
+        ("Meet the skibidis", "Big library of skibidi", "Yessir!", "static/images/logo.png")
+        ("Meet the skibidis", "Big library of skibidi", "Yessir!", "static/images/logo.png")
+    )
+    )
     return render_template("index.html"), 200
 
 if __name__ == '__main__':
