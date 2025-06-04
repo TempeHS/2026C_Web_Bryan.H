2353651b19f55d27d44fd3a3e6d42b8c4408c772
Bryan H <bryan.huang1@education.nsw.gov.au>
2025-06-04 01:55:21 +0000

    nothing

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/index.html')
 def index():
     card_data = (
-        ("Meet the skibidis", "Big library of skibidi", "Yessir!", "static/images/slider_1.png"),
-        ("Who are they?", "Big library of skibidi", "Yessir!", "static/images/logo.png"),
+        ("Meet the skibidis", "Big library of skibidi", "Yessir!", "static/images/slider_1.png", "/Library.html"),
+        ("Who are they?", "And I wonder", "Yessir!", "static/images/logo.png"),
         ("Meet the skibidis", "Big library of skibidi", "Yessir!", "static/images/logo.png"),
         ("Meet the skibidis", "Big library of skibidi", "Yessir!", "static/images/logo.png"),
         ("Meet the skibidis", "Big library of skibidi", "Yessir!", "static/images/logo.png")
@@ -16,6 +16,20 @@ def index():
     )
 
     return render_template("index.html", cards=card_data), 200
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html"), 200
+
+
+@app.route('/Library.html')
+def Library():
+    return render_template("Library.html"),
+
+@app.route('/mango.html')
+def mango():
+    return render_template("mango.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
